2f3a57184f7f39ba72bf11b3061d200963386dde
Letícia Oliveira <le.oliveiira@gmail.com>
2026-02-02 14:22:24 -0300

    Add files via upload

--- geometry_novo_com_profundidade.py
+++ geometry_novo_com_profundidade.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
-
 from abaqus import *
 from abaqusConstants import *
-
-import section
-import regionToolset
 import displayGroupMdbToolset as dgm
-import part
-import sys
-import os
-
-# from caeModules import *
-# from driverUtils import executeOnCaeStartup
-# executeOnCaeStartup()
 
 
 def Pipe(name_model, name_part, inner_radius, base_depth, top_depth, thickness):
 
     # depth = base_depth - top_depth
-    # Crianção do Sketch - Geometria do Revestimento
+    # Criancao do Sketch - Geometria do Revestimento
     sketch_name = '__profile__' + name_part
     s = mdb.models[name_model].ConstrainedSketch(
         name=sketch_name, sheetSize=10000.0)
     s.sketchOptions.setValues(viewStyle=AXISYM)
     s.setPrimaryObject(option=STANDALONE)
 
-    # Criação da Linha de Axisimetria
+    # Criacao da Linha de Axisimetria
     axis = s.ConstructionLine(point1=(0.0, -10000.0), point2=(0.0, 10000.0))
     s.FixedConstraint(entity=axis)
 
-    # Construção da Geometria do Revestimento
+    # Construcao da Geometria do Revestimento
     s.rectangle(point1=(inner_radius, -(top_depth)),
                 point2=(inner_radius + thickness, -(base_depth)))
 
@@ -43,20 +32,20 @@ def Pipe(name_model, name_part, inner_radius, base_depth, top_depth, thickness):
 def Fluid(name_model, name_part, inner_radius, base_depth, top_depth, thickness):
 
     # depth = base_depth - top_depth
-    # Criação do Sketch - Geometria do Revestimento
+    # Criacao do Sketch - Geometria do Revestimento
     sketch_name = '__profile__' + name_part
     s1 = mdb.models[name_model].ConstrainedSketch(
         name=sketch_name, sheetSize=1000.0)
     s1.sketchOptions.setValues(viewStyle=AXISYM)
     s1.setPrimaryObject(option=STANDALONE)
 
-    # Criação da Linha de Axisimetria
+    # Criacao da Linha de Axisimetria
     axis = s1.ConstructionLine(point1=(0.0, -10000.0), point2=(0.0, 10000.0))
     s1.FixedConstraint(entity=axis)
 
-    # Construção da Geometria do Fluido/Anular
+    # Construcao da Geometria do Fluido/Anular
     s1.rectangle(point1=(inner_radius, -(top_depth)),
-                point2=(inner_radius + thickness, -(base_depth)))
+                 point2=(inner_radius + thickness, -(base_depth)))
 
     p = mdb.models[name_model].Part(
         name=name_part, dimensionality=AXISYMMETRIC, type=DEFORMABLE_BODY)
@@ -68,66 +57,36 @@ def Fluid(name_model, name_part, inner_radius, base_depth, top_depth, thickness)
 def Rock(name_model, name_part, inner_radius, base_depth, top_depth, thickness):
 
     # depth = base_depth - top_depth
-    # Criação do Sketch - Geometria da Rocha
+    # Criacao do Sketch - Geometria da Rocha
     sketch_name = '__profile__' + name_part
     s2 = mdb.models[name_model].ConstrainedSketch(
         name=sketch_name, sheetSize=1000.0)
     s2.sketchOptions.setValues(viewStyle=AXISYM)
     s2.setPrimaryObject(option=STANDALONE)
 
-    # Criação da Linha de Axisimetria
+    # Criacao da Linha de Axisimetria
     axis = s2.ConstructionLine(point1=(0.0, -10000.0), point2=(0.0, 10000.0))
     s2.FixedConstraint(entity=axis)
 
-    # Construção da Geometria da Rocha
+    # Construcao da Geometria da Rocha
     s2.rectangle(point1=(inner_radius, -(top_depth)),
-                point2=(inner_radius + thickness, -(base_depth)))
+                 point2=(inner_radius + thickness, -(base_depth)))
 
-    p = mdb.models[name_model].Part(name=name_part, dimensionality=AXISYMMETRIC, type=DEFORMABLE_BODY)
+    p = mdb.models[name_model].Part(
+        name=name_part, dimensionality=AXISYMMETRIC, type=DEFORMABLE_BODY)
     p.BaseShell(sketch=s2)
     s2.unsetPrimaryObject()
-    # Criação dos Reference Points nas profundidades de topo
+    # Criacao dos Reference Points nas profundidades de topo
     s2.Line(point1=(0, top_depth), point2=(1000, top_depth))
     rp_top = p.ReferencePoint(point=(0.0, -(top_depth), 0.0))
-    p.Set(name='RP_TOP_%s' % top_depth, referencePoints=(p.referencePoints[rp_top.id], ))
-    # Criação dos Reference Points nas profundidades de base
+    p.Set(name='RP_TOP_%s' % top_depth, referencePoints=(
+        p.referencePoints[rp_top.id], ))
+    # Criacao dos Reference Points nas profundidades de base
     s2.Line(point1=(0, base_depth), point2=(1000, base_depth))
     # rp_base = p.ReferencePoint(point=(0.0, -(base_depth), 0.0))
     # p.Set(name='RP_BASE_%s' % base_depth, referencePoints=(p.referencePoints[rp_base.id], ))
-   
+
     return p
-
-# try:
-#     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# except NameError:
-#     Abaqus/CAE interactive mode
-#     BASE_DIR = os.getcwd()
-
-# if BASE_DIR not in sys.path:
-#     sys.path.insert(0, BASE_DIR)
-
-def PartitionLayersByDepth(model_name, part_name, layer_depths):
-    """
-    Cria partições horizontais (camadas) em uma Part axisimétrica
-    usando planos em profundidades especificadas.
-    """
-    model = mdb.models[model_name]
-    p = model.parts[part_name]
-
-    # Todas as faces do shell axisimétrico
-    faces = p.faces[:]
-
-    for depth in layer_depths:
-        y_coord = -depth  # convenção do seu modelo
-
-        # Cria plano datum horizontal
-        dp = p.DatumPlaneByPrincipalPlane(principalPlane=XZPLANE, offset=y_coord)
-        datum_id = dp.id
-
-        # Particiona todas as faces cortadas pelo plano
-        p.PartitionFaceByDatumPlane(datumPlane=p.datums[datum_id],
-                                    faces=faces
-                                    )
 
 
 def CreateGeometry(name_model, name, data):
@@ -150,20 +109,90 @@ def CreateGeometry(name_model, name, data):
                          )
     else:
         raise ValueError("Geometry type '%s' is not recognized." % name)
-      
+
+
+def PartitionLayersByDepth(modelName, part_name, layer_depths):
+    model = mdb.models[modelName]
+    p = model.parts[part_name]
+    datum_ids = []
+
+    # 1. Criacao dos Planos Datum
+    for depth in layer_depths:
+        y_coord = -float(depth)
+        dp = p.DatumPlaneByPrincipalPlane(
+            principalPlane=XZPLANE, offset=y_coord)
+        datum_ids.append(dp.id)
+
+    # 2. Partition - Re-seleciona faces a cada corte
+    for d_id in datum_ids:
+        # Seleciona todas as faces atuais
+        faces_to_partition = tuple(p.faces[:])
+        p.PartitionFaceByDatumPlane(datumPlane=p.datums[d_id],
+                                    faces=faces_to_partition)
+        p.regenerate()
+
+    print("Partitions concluded for part: {}".format(part_name))
+
+
+def CreateSetforLayers(modelName, part_name, layer_depths, top_depth, base_depth, inner_radius, thickness):
+    p = mdb.models[modelName].parts[part_name]
+    all_depths = sorted([top_depth, base_depth] + layer_depths)
+    # all_depths = sorted([part_data["top_depth"], part_data["base_depth"]] + part_data["layer_depths"])
+    layer_info = []
+
+    # Criar sets para cada camada
+    for i in range(len(all_depths) - 1):
+        # No sketch as coordenadas sao negativas
+        top = all_depths[i]
+        bottom = all_depths[i + 1]
+        mid_depth = -(top + bottom) / 2.0
+        mid_radius = inner_radius + thickness / 2.0
+
+        # Coordenadas X (raio)
+        # x_min = inner_radius
+        # x_max = inner_radius + thickness
+
+        # Nome unico: SET_ROCK_LAYER_PROF1_PROF2
+        set_name = 'SET_%s_Layer_%d_%d' % (part_name, int(top), int(bottom))
+
+        # Captura a face que contém o ponto médio da camada
+        faces_in_layer = p.faces.findAt(((mid_radius, mid_depth, 0.0), ))
+
+        if faces_in_layer:
+            p.Set(faces=faces_in_layer, name=set_name)
+            print("Set criado com sucesso: %s" % (set_name))
+        # else:
+        #     mid_y = (top + bottom) / 2.0
+        #     mid_x = (x_min + x_max) / 2.0
+        #     try:
+        #         f = p.faces.findAt(((mid_x, mid_y, 0.0), ))
+        #         p.Set(faces=p.faces[f.index:f.index+1], name=set_name)
+        #         print("Set criado via findAt: %s" % set_name)
+        #     except:
+        #         print("ERRO CRITICO: Nao foi possivel encontrar face para %s" % set_name)
+
+        layer_info.append({
+            "set_name": set_name,
+            "top_depth": top,
+            "base_depth": bottom
+        })
+
+    return layer_info
+
+
 if __name__ == "__main__":
     example = {
         "pipe_inner_radius": 0.11,
         "annular_radius": 0.13,
         "rock_radius": 0.14,
-        "base_depth": 3600,
+        "base_depth": 4000,
         "top_depth": 3200,
         "pipe_wt": 0.02,
         "fluid_wt": 0.01,
         "rock_wt": 14.87
     }
 
-    layers_depths = [3300, 3400, 3500]
+    layers_depths = [3300, 3550, 3600]
 
     data = {
         "ROCK": {"inner_radius": example["rock_radius"],
@@ -186,12 +215,17 @@ if __name__ == "__main__":
                  "layer_depths": layers_depths
                  },
     }
-    
-    if 'MyFirstModel' not in mdb.models:
-        mdb.Model(name='MyFirstModel', modelType=STANDARD_EXPLICIT)
-    for part_name, part_data in data.items():
-        CreateGeometry('MyFirstModel', part_name, part_data)
-        PartitionLayersByDepth("MyFirstModel", part_name=part_name, layer_depths=part_data["layer_depths"])
 
-print('Geometria criada em:', mdb.models['MyFirstModel'])
+    name_model = 'MyFirstModel'
+    if name_model not in mdb.models:
+        mdb.Model(name=name_model, modelType=STANDARD_EXPLICIT)
+    for part_name, part_data in data.items():
+        CreateGeometry(name_model, part_name, part_data)
+    # if part_name == 'ROCK':
+        PartitionLayersByDepth(
+            name_model, part_name=part_name, layer_depths=part_data["layer_depths"])
+        layers_info = CreateSetforLayers(name_model, part_name=part_name, layer_depths=part_data["layer_depths"],
+                                         top_depth=part_data["top_depth"], base_depth=part_data["base_depth"],
+                                         inner_radius=part_data["inner_radius"], thickness=part_data["thickness"])
+    # print('Geometry created in:', mdb.models['MyFirstModel'])
 mdb.saveAs(pathName='FEM_AXYS_WELL_SHIELD.cae')
